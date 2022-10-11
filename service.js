477c2f669177771b4d0322baab5a75fcd757aa66
devazeez <l.adeosun.la@gmail.com>
2022-10-11 11:26:41 +0100

    Made a few media queries on service screen

---
+++ service.js
@@ -0,0 +1,29 @@
+// function myFunction() {
+//  var link = document.getElementByClassName("hamburger")
+//  if( link.style.display === "flex"){
+//     link.style.display = "none"
+//  } else{
+//     link.style.display = "flex";
+//  }
+// }
+function myFunction() {
+    // document.getElementById('btnn').
+    var link = document.getElementById("hamburger")
+    if (link.style.display === "flex") {
+        link.style.display = "none"
+    } else {
+        link.style.display = "flex";
+    }
+    
+}
+
+// document.getElementById("iconn").onClick = function() {
+// alert('you clicked me')
+
+// }
+
+
+//    document.getElementsById('iconn').onclick = function()
+//    {
+//        alert('you clicked me');
+//    }    
